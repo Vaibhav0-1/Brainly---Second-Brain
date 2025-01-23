56321bb360a066dce659d78d2ff84d4cf8586826
Vaibhav <vaibhav.sharma3100@gmail.com>
2025-01-23 17:53:49 +0530

    endpoint - signin

--- dist/index.js
+++ dist/index.js
@@ -13,9 +13,11 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const jsonwebtoken_1 = __importDefault(require("jsonwebtoken"));
 const db_1 = require("./db");
 const app = (0, express_1.default)();
 app.use(express_1.default.json());
+const JWT_PASSWORD = "12345";
 app.post("/api/v1/signup", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     //zod validation, hashing the password
     const username = req.body.username;
@@ -35,8 +37,27 @@ app.post("/api/v1/signup", (req, res) => __awaiter(void 0, void 0, void 0, funct
         });
     }
 }));
-app.post("/api/v1/signin", (req, res) => {
-});
+app.post("/api/v1/signin", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const username = req.body.username;
+    const password = req.body.password;
+    const existingUser = yield db_1.UserModel.findOne({
+        username,
+        password
+    });
+    if (existingUser) {
+        const token = jsonwebtoken_1.default.sign({
+            id: existingUser._id
+        }, JWT_PASSWORD);
+        res.json({
+            token
+        });
+    }
+    else {
+        res.status(403).json({
+            message: "Invalid username or password"
+        });
+    }
+}));
 app.post("/api/v1/content", (req, res) => {
 });
 app.get("/api/v1/content", (req, res) => {
